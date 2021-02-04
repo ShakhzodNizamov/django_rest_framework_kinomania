90c3d6d06167e581314e0b5c97f3d1cf16e8b15d
Jaga <shaxzod.nizomov@mail.ru>
2021-02-04 10:44:28 +0500

    - Ability to post review for movies - The ability to get reviews of detailed description of the movie

--- kinoman/serializers.py
+++ kinoman/serializers.py
@@ -1,6 +1,22 @@
 from rest_framework import serializers
 
-from .models import Movie
+from .models import Movie, Review
+
+
+class FilterReviewListSerializer(serializers.ListSerializer):
+    """Фильтр комментариев, только parents"""
+
+    def to_representation(self, data):
+        data = data.filter(parent=None)
+        return super().to_representation(data)
+
+
+class RecursiveSerializer(serializers.Serializer):
+    """Вывод рекурсивно children"""
+
+    def to_representation(self, value):
+        serializer = self.parent.parent.__class__(value, context=self.context)
+        return serializer.data
 
 
 class MovieListSerializer(serializers.ModelSerializer):
@@ -11,12 +27,31 @@ class MovieListSerializer(serializers.ModelSerializer):
         fields = ('id', 'title', 'tagline', 'category')
 
 
+class ReviewCreateSerializer(serializers.ModelSerializer):
+    """Добавление отзывов"""
+
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    """Вывод отзыва"""
+    children = RecursiveSerializer(many=True)
+
+    class Meta:
+        list_serializer_class = FilterReviewListSerializer
+        model = Review
+        fields = ('name', 'text', 'children')
+
+
 class MovieDetailSerializer(serializers.ModelSerializer):
     """Детальное описание Фильма"""
     category = serializers.SlugRelatedField(slug_field='name', read_only=True)
     directors = serializers.SlugRelatedField(slug_field='name', read_only=True, many=True)
     actors = serializers.SlugRelatedField(slug_field='name', read_only=True, many=True)
     genres = serializers.SlugRelatedField(slug_field='name', read_only=True, many=True)
+    reviews = ReviewSerializer(many=True)
 
     class Meta:
         model = Movie
--- kinoman/urls.py
+++ kinoman/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('movies/', views.MovieListView.as_view()),
-    path('movie/<int:primary_key>/', views.MovieDetailView.as_view())
+    path('movie/<int:primary_key>/', views.MovieDetailView.as_view()),
+    path('review/', views.ReviewCreateView.as_view()),
 ]
