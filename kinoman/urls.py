701fdde016d956691f0aac44753fc95457eba6a5
Jaga <shaxzod.nizomov@mail.ru>
2021-02-10 23:46:58 +0500

    - Changed APIView to generics APIs in view.py

--- kinoman/urls.py
+++ kinoman/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('movies/', views.MovieListView.as_view()),
-    path('movie/<int:primary_key>/', views.MovieDetailView.as_view()),
+    path('movie/<int:pk>/', views.MovieDetailView.as_view()),
     path('review/', views.ReviewCreateView.as_view()),
     path("rating/", views.AddStarRatingView.as_view()),
     path("actors/", views.ActorsListView.as_view()),
